06a2a70586fba607dd6284fe9dbc953b514cbaf5
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-03-13 15:12:45 +0200

    Added class helper for Database User for ToString method

---
+++ DataLayer/Helpers/DatabaseUserHelper.cs
@@ -0,0 +1,21 @@
+﻿using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+
+namespace DataLayer.Helpers
+{
+    public static class DatabaseUserHelper
+    {
+        public static string ToString(this DatabaseUser user)
+        {
+            var subjectsList = user.DatabaseSubjects?.Select(s => s.Name) ?? Enumerable.Empty<string>();
+            var subjectsString = string.Join(", ", subjectsList);
+
+            return $"{user.Id}. Name: {user.Name}, Email: {user.Email}, Faculty Number: {user.FacultyNumber}, Role: {user.Role}, Subjects: [{subjectsString}]";
+        }
+    }
+}
