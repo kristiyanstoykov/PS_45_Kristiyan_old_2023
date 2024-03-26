872e8d96454f0fb750b142f8975f69035dbd2889
Kristiyan Stoykov <krisistoikov@gmail.com>
2024-03-26 14:23:19 +0200

    Updated properties of ObservableObject with raisng proeprty chenged event. Added SubjectName and filtration by subject name with ICommand

--- UI/ViewModel/Presenter.cs
+++ UI/ViewModel/Presenter.cs
@@ -7,23 +7,69 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace UI.ViewModel
 {
     public class Presenter : ObservableObject
     {
-        public ObservableCollection<DatabaseUser> Users { get; set; } = new ObservableCollection<DatabaseUser>();
+        public string SubjectName { get; set; }
+        private ObservableCollection<DatabaseUser> _users = new ObservableCollection<DatabaseUser>();
 
-        public ObservableCollection<DatabaseLogger> Logger { get; set; } = new ObservableCollection<DatabaseLogger>();
+        public ObservableCollection<DatabaseUser> Users
+        {
+            get => _users;
+            set
+            {
+                if (_users != value)
+                {
+                    _users = value;
+                    RaisePropertyChangedEvent(nameof(Users)); // Notify the UI of the change
+                }
+            }
+        }
+
+        public ObservableCollection<DatabaseLogger> _logger = new ObservableCollection<DatabaseLogger>();
+        public ObservableCollection<DatabaseLogger> Logger
+        {
+            get => _logger;
+            set
+            {
+                if (_logger != value)
+                {
+                    _logger = value;
+                    RaisePropertyChangedEvent(nameof(_logger)); // Notify the UI of the change
+                }
+            }
+        }
+
+        public ICommand FilterCommand
+        {
+            get
+            {
+                return new DelegateCommand(FilterUsersBySubject);
+            }
+        }
 
         public Presenter()
         {
+            SubjectName = string.Empty;
             LoadUsers();
             LoadLogger();
         }
 
         private void LoadUsers()
         {
+            using var context = new DatabaseContext();
+            var records = context.Users.Include(s => s.DatabaseSubjects).ToList();
+            foreach (var user in records)
+            {
+                Users.Add(user);
+            }
+        }
+        private void ReLoadUsers()
+        {
+            Users.Clear();
             using var context = new DatabaseContext();
             var records = context.Users.Include(s => s.DatabaseSubjects).ToList();
             foreach (var user in records)
@@ -41,5 +87,27 @@ namespace UI.ViewModel
                 Logger.Add(log);
             }
         }
+
+        public void FilterUsersBySubject()
+        {
+            if (string.IsNullOrWhiteSpace(SubjectName))
+            {
+                ReLoadUsers();
+                return;
+            }
+
+            using var context = new DatabaseContext();
+            var filtered = context.Users
+                                   .Include(s => s.DatabaseSubjects)
+                                   .Where(user => user.DatabaseSubjects.Any(s => s.Name == SubjectName))
+                                   .ToList();
+
+            Users.Clear();
+            foreach (var user in filtered)
+            {
+                Users.Add(user);
+            }
+        }
+
     }
 }
